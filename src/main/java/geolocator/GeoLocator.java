909d0f0d123007547baa0bb3ce91bdee5cd4e11b
Péter Jeszenszky <jeszenszky.peter.deik@gmail.com>
2021-02-28 17:17:09 +0100

    Added Javadoc comments

--- src/main/java/geolocator/GeoLocator.java
+++ src/main/java/geolocator/GeoLocator.java
@@ -11,18 +11,43 @@ import com.google.common.net.UrlEscapers;
 
 import org.apache.commons.io.IOUtils;
 
+/**
+ * Class for obtaining geolocation information of an IP address. The class uses
+ * the <a href="http://ip-api.com/">IP-API.com</a> service.
+ */
 public class GeoLocator {
 
+    /**
+     * URI of the geolocation service.
+     */
     public static final String GEOLOCATOR_SERVICE_URI = "http://ip-api.com/json/";
 
     private static ObjectMapper OBJECT_MAPPER = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
+    /**
+     * Creates a {@code GeoLocator} object.
+     */
     public GeoLocator() {}
 
+    /**
+     * Returns geolocation information about the JVM running the application.
+     *
+     * @return an object wrapping the geolocation information returned
+     * @throws IOException if any I/O error occurs
+     */
     public GeoLocation getGeoLocation() throws IOException {
         return getGeoLocation(null);
     }
 
+    /**
+     * Returns geolocation information about the IP address or host name specified.
+     * If the argument is {@code null}, the method returns geolocation information
+     * about the JVM running the application.
+     *
+     * @param ipAddrOrHost the IP address or host name, may be {@code null}
+     * @return an object wrapping the geolocation information returned
+     * @throws IOException if any I/O error occurs
+     */
     public GeoLocation getGeoLocation(String ipAddrOrHost) throws IOException {
         URL url;
         if (ipAddrOrHost != null) {
--- src/main/lombok/geolocator/GeoLocation.java
+++ src/main/lombok/geolocator/GeoLocation.java
@@ -1,5 +1,8 @@
 package geolocator;
 
+/**
+ * Class for wrapping geolocation information about an IP address.
+ */
 @lombok.Data
 public class GeoLocation {
 
