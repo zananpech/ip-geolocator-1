05ab156a2970506ee636aabe9651008b339845da
Zanan Pech <zananpech9@gmail.com>
2021-03-13 21:28:08 +0100

    add Geolocation class

---
+++ src/main/java/geolocator/GeoLocation.java
@@ -0,0 +1,18 @@
+package geolocator;
+
+public record GeoLocation(
+        String as,
+        String city,
+        String country,
+        String countryCode,
+        String isp,
+        double lat,
+        double lon,
+        String org,
+        String query,
+        String region,
+        String regionName,
+        String timezone,
+        String zip
+) {
+}
